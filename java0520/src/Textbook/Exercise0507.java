56ce6514afd29a1b8cc02496cd17783fdc6d48d9
synetcom1 <tech.headinstructor@ciccc.ca>
2020-06-22 16:38:00 -0700

    Exercise 0507

--- java0520/src/Textbook/Exercise0507.java
+++ java0520/src/Textbook/Exercise0507.java
@@ -7,12 +7,19 @@ public class Exercise0507 {
 		int totalCost = 0;
 		int tuition = 10000;
 		int tuitionTenthYear = 0;
-//		for(int i =1; i<=14; i++)
+		for (int year = 1; year <= 14; year++) {
+//			Increase the tuition 5% each year
+			tuition += (tuition * 0.05);
 
-//		if(year > 10)
-		// totalCost += tuition;
-//		if(year == 10)
-//			tuitionTenthYear = tuition;
+			if (year > 10) // after 10 years
+				totalCost += tuition; // add all the tuitions from 11th to 14th
+			if (year == 10)
+				tuitionTenthYear = tuition;
+		}
+//		tuition of 10 years
+		System.out.println("The tuition in 10 years : $" + tuitionTenthYear);
+//	Display 4 years of tuition after 10th year
+		System.out.println("THe total cost for four years' worth of tuition after 10th year $" + totalCost);
 
 	}
 
---
+++ java0520/src/Textbook/Exercise0508.java
@@ -0,0 +1,21 @@
+package Textbook;
+
+public class Exercise0508 {
+
+	public static void main(String[] args) {
+		int highestScore = 0;
+		String highstScoreName = "";
+
+		// int numberOfStudents = input.nextInt()
+		// input.next() --> student's name
+		// input.netInt() --> student's score
+
+//
+//		if (score > highestScore) {
+//			highestScore = score;
+//			highestScoreName = name;
+//		}
+
+	}
+
+}
